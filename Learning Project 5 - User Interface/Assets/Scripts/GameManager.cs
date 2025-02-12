955722bd2905a19b068b1bf89b76e01f49a1329d
M. Sameel Khan <sameelkhan2010@gmail.com>
2025-02-12 20:45:12 +0500

    Learn.Unity Project 3

---
+++ Learning Project 5 - User Interface/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.5f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool isGameActive;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }      
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd; 
+        scoreText.text = "Score : " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        spawnRate/=difficulty;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+}
